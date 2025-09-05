7508aa64eba7cda5805e8c0b18a1ec0fe8f9f583
klasik54 <matus.klasovity@gmail.com>
2025-09-05 10:55:29 +0200

    feat: Update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 // vite.config.js
 
+import { resolve } from 'path'
+
 export default {
     root: 'src',
     build: {
         outDir: '../dist',
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'src/index.html'),
+                privacy: resolve(__dirname, 'src/photo-to-pdf-privacy-policy.html')
+            }
+        }
     }
 }
