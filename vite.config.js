efec2d03556a9902d0f6175a7f35c40d376723d7
klasik54 <matus.klasovity@gmail.com>
2026-02-05 14:52:03 +0100

    Add privacy policy page for Phasio

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,8 @@ export default {
             input: {
                 main: resolve(__dirname, 'src/index.html'),
                 photoToPdfPrivacyPolicy: resolve(__dirname, 'src/photo-to-pdf-privacy-policy.html'),
-                nhiePrivacyPolicy: resolve(__dirname, 'src/nhie-privacy-policy.html')
+                nhiePrivacyPolicy: resolve(__dirname, 'src/nhie-privacy-policy.html'),
+                phasioPrivacyPolicy: resolve(__dirname, 'src/phasio-privacy-policy.html')
             }
         }
     }
